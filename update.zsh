5c02e5eac11366c84eeef5a9eac2a226b7dec41d
Elija Sorensen <eli.r.sorensen@gmail.com>
2016-04-14 13:47:50 -0400

    fixed title and incorrect div nesting

--- update.zsh
+++ update.zsh
@@ -1,5 +1,7 @@
 #!/bin/env zsh
 
+#must be run from somewhere inside the dgdc directory due to limitations of middleman build command (it can't be pointed at a path, only looks in current location)
+
 middleman build
 \rm -rf /var/www/andysorensen.com/public_html/*
 cp -r /var/www/andysorensen.com/dgdc/build/* /var/www/andysorensen.com/public_html
